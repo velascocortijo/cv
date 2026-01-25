b4757bdef657ac476b3d28978b569b9a8201f100
velascocortijo <velascocortijo@gmail.com>
2026-01-25 17:16:34 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 // Configuración Global - Reemplaza con tus IDs reales
 const CONFIG = {
     GOOGLE_CLIENT_ID: "TU_GOOGLE_CLIENT_ID_AQUI.apps.googleusercontent.com",
-    SPREADSHEET_ID: "TU_ID_DE_GOOGLE_SHEET_AQUI",
+    SPREADSHEET_ID: "1L67JCrwGVxGuGghFgoZxM_zIXs0WP6bW4vjsOInvxu0",
     DRIVE_FOLDER_ID: "TU_ID_DE_CARPETA_DRIVE_AQUI",
     AUTHORIZED_EMAILS: [
         "admin@example.com", // Tú (Admin)
@@ -16,3 +16,4 @@ const CONFIG = {
 if (typeof module !== 'undefined') {
     module.exports = CONFIG;
 }
+
