962aeebe09a8deb93580d49f756d6c64a1425895
velascocortijo <velascocortijo@gmail.com>
2026-03-07 06:32:44 +0100

    Add AUTHORIZED_EMAILS to global configuration

--- settings.js
+++ settings.js
@@ -3,9 +3,15 @@ const CONFIG = {
     GOOGLE_CLIENT_ID: "699107819652-19skv1kl2iu531qqoigpdd91jt9c26gj.apps.googleusercontent.com",
     SPREADSHEET_ID: "1L67JCrwGVxGuGghFgoZxM_zIXs0WP6bW4vjsOInvxu0",
     DRIVE_FOLDER_ID: "1_kocEDazNFHg191c9obZNu58ivmXimqL",
-    FAMILY_MEMBERS: ["Antonio", "Angelita", "Rebeca", "Raquel", "Jorge", "Tete", "Otros"],
-    INCOME_CATEGORIES: ["Alquiler", "Aportación Familiar", "Subvención", "Otros"]
+    AUTHORIZED_EMAILS: [
+        "joseangelgabriel@gmail.com", // Tú (Admin)
+        "primo1@example.com",
+        "primo2@example.com",
+        "primo3@example.com"
+        // Añade aquí los correos de tus primos
+    ]
 };
+
 // Exportar para que app.js lo use
 if (typeof module !== 'undefined') {
     module.exports = CONFIG;
