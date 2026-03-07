7ef5005cc0d12827526f8ae76847cb028a21a868
velascocortijo <velascocortijo@gmail.com>
2026-03-08 00:06:46 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,9 @@ const CONFIG = {
     GOOGLE_CLIENT_ID: "699107819652-19skv1kl2iu531qqoigpdd91jt9c26gj.apps.googleusercontent.com",
     SPREADSHEET_ID: "1L67JCrwGVxGuGghFgoZxM_zIXs0WP6bW4vjsOInvxu0",
     DRIVE_FOLDER_ID: "1_kocEDazNFHg191c9obZNu58ivmXimqL",
-    AUTHORIZED_EMAILS: [
-        "joseangelgabriel@gmail.com", // Tú (Admin)
-        "primo1@example.com",
-        "primo2@example.com",
-        "primo3@example.com"
-        // Añade aquí los correos de tus primos
-    ]
+    FAMILY_MEMBERS: ["Antonio", "Angelita", "Rebeca", "Raquel", "Jorge", "Tete", "Otros"],
+    INCOME_CATEGORIES: ["Alquiler", "Aportación Familiar", "Subvención", "Otros"]
 };
-
 // Exportar para que app.js lo use
 if (typeof module !== 'undefined') {
     module.exports = CONFIG;
