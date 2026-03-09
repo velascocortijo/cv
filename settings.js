3477bb2f57aae0c0edb8851f404775e5e8094ab9
velascocortijo <velascocortijo@gmail.com>
2026-03-09 01:53:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,10 +4,21 @@ const CONFIG = {
     SPREADSHEET_ID: "1L67JCrwGVxGuGghFgoZxM_zIXs0WP6bW4vjsOInvxu0",
     DRIVE_FOLDER_ID: "1_kocEDazNFHg191c9obZNu58ivmXimqL",
     FAMILY_MEMBERS: ["Antonio", "Angelita", "Rebeca", "Raquel", "Jorge", "Tete", "Otros"],
-    INCOME_CATEGORIES: ["Alquiler", "Aportación Familiar", "Subvención", "Otros"]
+    INCOME_CATEGORIES: ["Alquiler", "Aportación Familiar", "Subvención", "Otros"],
+    INVENTORY_CATEGORIES: [
+        "Mobiliario interior", "Electrodomésticos", "Menaje de Cocina",
+        "Ropa de cama y baño", "Limpieza y mantenimiento", "Mobiliario exterior",
+        "Piscina", "Seguridad", "Decoración y complementos",
+        "Suministros para huéspedes", "Almacén propio Pajar"
+    ],
+    INVENTORY_LOCATIONS: [
+        "Salón/Comedor", "Cocina", "Habitación 1 Cati", "Habitación 2 Lili",
+        "Habitación 3 Antonio", "Habitación 4 Angelita", "Baño 1", "Baño 2",
+        "Patio interior", "Patio exterior", "Piscina",
+        "Cuarto de depuradora", "Almacén propio / Pajar"
+    ]
 };
 window.CONFIG = CONFIG;
-
 // Exportar para que app.js lo use
 if (typeof module !== 'undefined') {
     module.exports = CONFIG;
