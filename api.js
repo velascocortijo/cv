e94dd19515f3593e9301a72c9beabb39ebdef105
velascocortijo <velascocortijo@gmail.com>
2026-03-30 02:38:11 +0200

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
  * API.JS - CLIENTE PARA EL BACKEND DEL CORTIJO VELASCO
  */
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbzKJuZgxKyz9J5In2Tym9BuBtItgt4rLMI4FNFB9b94hbXrIbzdVP56VDjSswhngJsN/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyqU-xLl3xfP7EEL7Cg17OtRtnORDkTmA8D9XXuB-SVSkgHuUBQGvHE1AiS-J2n6Iuh/exec';
 
 // --- WRAPPER SEGURO DE PETICIONES ---
 const fetchAPI = async (url, options) => {
