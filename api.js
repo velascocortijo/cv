bc659fb028da1e21966447ac7f4af9ea594fb78f
velascocortijo <velascocortijo@gmail.com>
2026-04-07 01:39:47 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // VERSION: V3.0.0 - Cliente API Single Source of Truth
-const API_URL = 'https://script.google.com/macros/s/AKfycbxdFtYAA9dnLEvcLSkcl06pBW5uhlH_xtT76fdnzm18Z1Ssjx06-YlkGIn6t31sCbnp/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxR1VtNHHn6JQNcysbHDqDnRS-O03zSnYeBjzHxDiIL8b1fHAgkLWWT2NB8Jn6bb81V/exec';
 
 const API = {
     // --- AUTENTICACIÓN ---
